dc1fea581800705f5a110333fe83aba1e66f8169
root <root@LAPTOP-8O10EN7G>
2025-03-18 15:04:45 +0800

    replace sqlite with postgre

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,13 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config["JWT_SECRET_KEY"] = f"{app.config['SECRET_KEY']}"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://test:password@localhost/test"
+
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
 from app import models
 from app import routes
+
+with app.app_context():
+    db.create_all()
